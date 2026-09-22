1b1b1a537c5faba6d0243ad3e8dba69dde0eaa91
Manavjeet Gupta <manaviitp123@gmail.com>
2026-09-22 11:36:21 +0530

    fix(auth): import size modifier for signup

--- android/app/src/main/java/com/recharge/client/features/auth/RegisterScreen.kt
+++ android/app/src/main/java/com/recharge/client/features/auth/RegisterScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.size
 import com.recharge.client.core.theme.AppColors
 import com.recharge.client.core.viewmodel.AuthUiState
 
