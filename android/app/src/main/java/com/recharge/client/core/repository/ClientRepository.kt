bf2a3fafe55ca2e9d17b3283f93eea85b7290b44
Manavjeet Gupta <manaviitp123@gmail.com>
2026-09-22 19:57:34 +0530

    fix(android): import rental availability models

--- android/app/src/main/java/com/recharge/client/core/repository/ClientRepository.kt
+++ android/app/src/main/java/com/recharge/client/core/repository/ClientRepository.kt
@@ -21,6 +21,9 @@ import com.recharge.client.core.model.WalletHistoryResponse
 import com.recharge.client.core.model.WithdrawMoneyRequest
 import com.recharge.client.core.model.WithdrawMoneyResponse
 import com.recharge.client.core.model.WithdrawalHistoryResponse
+import com.recharge.client.core.model.RentalVehicleCalendarResponse
+import com.recharge.client.core.model.RentalVehicleUnavailabilityResponse
+import com.recharge.client.core.model.RentalVehicleUnavailabilityRequest
 import com.recharge.client.core.model.RentalBookingQuoteRequest
 import com.recharge.client.core.model.RentalBookingQuoteResponse
 import com.recharge.client.core.model.ProfileUpdateRequest
