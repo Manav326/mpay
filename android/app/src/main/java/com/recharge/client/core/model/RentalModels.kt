2065f71e33116634845d0ae43bea3296bca9e0e5
Manavjeet Gupta <manaviitp123@gmail.com>
2026-09-22 13:53:57 +0530

    Add rental booking pagination model

--- android/app/src/main/java/com/recharge/client/core/model/RentalModels.kt
+++ android/app/src/main/java/com/recharge/client/core/model/RentalModels.kt
@@ -95,3 +95,5 @@ data class RentalBookingResponse(
     val status: String,
     val createdAt: String
 )
+
+data class RentalBookingPageResponse(val items: List<RentalBookingResponse>, val page: Int, val size: Int, val totalItems: Long, val totalPages: Int, val hasNext: Boolean)
