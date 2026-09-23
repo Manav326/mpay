c90b9c68ada76486a18e688f3c2d2580271c9eb1
Manavjeet Gupta <manaviitp123@gmail.com>
2026-09-23 19:12:08 +0530

    fix(android): preserve session reset compilation

--- android/app/src/main/java/com/recharge/client/core/viewmodel/HomeViewModel.kt
+++ android/app/src/main/java/com/recharge/client/core/viewmodel/HomeViewModel.kt
@@ -7,6 +7,7 @@ import com.recharge.client.core.model.CurrentUserResponse
 import com.recharge.client.core.model.WalletResponse
 import com.recharge.client.core.repository.ClientRepository
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
