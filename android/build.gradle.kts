7495fc918ba0522aa2ea695628f587422836c8e5
Manavjeet Gupta <manaviitp123@gmail.com>
2026-09-25 08:30:55 +0530

    android: remove unnecessary secrets plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
