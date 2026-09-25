c7099f2ee11717a9a4183d6ad00cae548fa3a385
Manavjeet Gupta <manaviitp123@gmail.com>
2026-09-25 12:27:53 +0530

    fix(android): import KeyboardOptions from foundation text

--- android/app/src/main/java/com/recharge/client/features/rental/RentalScreens.kt
+++ android/app/src/main/java/com/recharge/client/features/rental/RentalScreens.kt
@@ -44,7 +44,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.draw.clip
